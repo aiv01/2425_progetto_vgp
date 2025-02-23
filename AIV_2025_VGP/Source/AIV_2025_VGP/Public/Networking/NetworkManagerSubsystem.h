9b2943996bf01117a9a4abfd5fdc7d037e8e14a8
alex28931 <alex28931@gmail.com>
2025-02-23 15:49:26 +0100

    Set build.cs and Network Manager Subsystem created

---
+++ AIV_2025_VGP/Source/AIV_2025_VGP/Public/Networking/NetworkManagerSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "OnlineSubsystem.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "NetworkManagerSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIV_2025_VGP_API UNetworkManagerSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+};
