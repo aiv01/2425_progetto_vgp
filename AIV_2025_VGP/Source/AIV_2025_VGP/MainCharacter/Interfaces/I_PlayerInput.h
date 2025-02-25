6cf3fd996dac205c06222e8a7e308b47f1ac226b
FabioGiannino <fabiogiannino2@gmail.com>
2025-02-25 18:33:28 +0100

    Add Base Player and IPlayer Interface

---
+++ AIV_2025_VGP/Source/AIV_2025_VGP/MainCharacter/Interfaces/I_PlayerInput.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "I_PlayerInput.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UI_PlayerInput : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class AIV_2025_VGP_API II_PlayerInput
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+};
