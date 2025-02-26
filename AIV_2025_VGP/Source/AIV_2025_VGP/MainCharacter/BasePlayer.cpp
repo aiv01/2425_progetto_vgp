0cb362eb2798bd0962931f15d3ccd084df3c798a
FabriSAM <fabri.conni@gmail.com>
2025-02-26 19:20:40 +0100

    Base Player Move Fix
    Fixed movement of player
    Lock player's rotation to camera's rotation

--- AIV_2025_VGP/Source/AIV_2025_VGP/MainCharacter/BasePlayer.cpp
+++ AIV_2025_VGP/Source/AIV_2025_VGP/MainCharacter/BasePlayer.cpp
@@ -27,7 +27,7 @@ ABasePlayer::ABasePlayer()
 	bUseControllerRotationRoll = false;
 
 	// Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	GetCharacterMovement()->bOrientRotationToMovement = false; // Character moves in the direction of input...	
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
 
 	// Note: For faster iteration times these variables, and many more, can be tweaked in the Character Blueprint
@@ -141,6 +141,9 @@ void ABasePlayer::Look_Implementation(const FVector2D& Value)
 	
 	if (Controller != nullptr)
 	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		SetActorRotation(YawRotation);
 		// add yaw and pitch input to controller
 		AddControllerYawInput(Value.X);
 		AddControllerPitchInput(Value.Y);
