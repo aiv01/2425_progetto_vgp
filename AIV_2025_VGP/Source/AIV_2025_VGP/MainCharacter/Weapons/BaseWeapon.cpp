19ae5fac289c7ac857042f552cbf5fe0ec7e7259
lucacasamentigamedev <luca.casamenti.gamedev@gmail.com>
2025-03-11 22:52:53 +0100

    player controller attacks
    renaming
    bullets
    montage/seuqence

--- AIV_2025_VGP/Source/AIV_2025_VGP/MainCharacter/Weapons/BaseWeapon.cpp
+++ AIV_2025_VGP/Source/AIV_2025_VGP/MainCharacter/Weapons/BaseWeapon.cpp
@@ -11,8 +11,11 @@ ABaseWeapon::ABaseWeapon()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false; 
 
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
 	WeaponMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("WeaponMesh"));
-	RootComponent = WeaponMesh;
+	//RootComponent = WeaponMesh;
+	WeaponMesh->SetupAttachment(RootComponent);
 }
 
 // Called when the game starts or when spawned
