e3850d652e8bea922fce76a8776af88915e740bb
marcopungillodev <pungillomarco@gmail.com>
2025-03-18 14:06:34 +0100

    Agents and Actions organization
    ADD:
    • Blueprint interface and empty (by now) C++ interface for GOAP operations on agent;
    • Action inizialization
    • Reference to splinePath in agent, so that the GOAP actions can get it from the executer
    
    CLEAN:
    • Workspace organization

---
+++ AIV_2025_VGP/Source/AIV_2025_VGP/Private/I_GOAP_Agent.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "I_GOAP_Agent.h"
+
+// Add default functionality here for any II_GOAP_Agent functions that are not pure virtual.
