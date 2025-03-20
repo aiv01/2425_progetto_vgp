c44c9e424516e3027f2c14269cf92b3ba0e43763
marcopungillodev <pungillomarco@gmail.com>
2025-03-20 12:33:44 +0100

    Copyright
    ADD:
    • Copyright sign

--- AIV_2025_VGP/Source/AIV_2025_VGP/GOAPGoalDefiner.cpp
+++ AIV_2025_VGP/Source/AIV_2025_VGP/GOAPGoalDefiner.cpp
@@ -1,4 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
+// COPYRIGHT
+// Marco Pungillo
 
 #include "GOAPGoalDefiner.h"
 #include "BaseEnemy.h"
--- AIV_2025_VGP/Source/AIV_2025_VGP/Private/I_GOAP_Agent.cpp
+++ AIV_2025_VGP/Source/AIV_2025_VGP/Private/I_GOAP_Agent.cpp
@@ -1,5 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
+// COPYRIGHT
+// Marco Pungillo
 
 #include "I_GOAP_Agent.h"
 
