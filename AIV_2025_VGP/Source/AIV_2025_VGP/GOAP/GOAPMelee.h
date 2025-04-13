16fae15923696e1001d240d1d6741f6676495d7e
cherrybloom002 <161952780+cherrybloom002@users.noreply.github.com>
2025-04-13 23:38:31 +0200

    Adjusted GOAPStrategies to get animation sequances

--- AIV_2025_VGP/Source/AIV_2025_VGP/GOAP/GOAPMelee.h
+++ AIV_2025_VGP/Source/AIV_2025_VGP/GOAP/GOAPMelee.h
@@ -14,7 +14,7 @@ class AIV_2025_VGP_API UGOAPMelee : public UGOAPBaseCombatStrategy
 
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Params")
-	UAnimMontage* AnimationToPlay;
+	UAnimSequence* AnimationToPlay;
 
 	virtual void Execute() override;
 
