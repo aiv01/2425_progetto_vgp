fec664f67a20103421ff56d3321e435b4a5eeec5
cherrybloom002 <161952780+cherrybloom002@users.noreply.github.com>
2025-04-13 23:38:31 +0200

    Adjusted GOAPStrategies to get animation sequances

--- AIV_2025_VGP/Source/AIV_2025_VGP/GOAP/Strategy/GOAPSwordStrategy.cpp
+++ AIV_2025_VGP/Source/AIV_2025_VGP/GOAP/Strategy/GOAPSwordStrategy.cpp
@@ -1,5 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
+// Andreea Manuela Hodor
 
 #include "AIV_2025_VGP/GOAP/Strategy/GOAPSwordStrategy.h"
 #include "GameFramework/Character.h"
